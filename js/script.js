2460aade868eda8bb0d4327dddb34c277f6eb676
Jauvas <143481214+Jauvas@users.noreply.github.com>
2024-05-15 08:24:52 +0300

    3

--- js/script.js
+++ js/script.js
@@ -13,3 +13,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
     setInterval(nextSlide, 3000);
 });
+
+
+function openModal(imageUrl) {
+    var modal = document.getElementById("modal");
+    var modalImg = document.getElementById("modal-img");
+    modal.style.display = "block";
+    modalImg.src = imageUrl;
+}
+
+function closeModal() {
+    var modal = document.getElementById("modal");
+    modal.style.display = "none";
+}
+
+
